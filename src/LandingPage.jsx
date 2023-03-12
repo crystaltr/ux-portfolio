f0ce1daef80a02e114832db7389bd494d9d4242f
crystaltr <ctru0002@student.monash.edu>
2023-03-12 19:24:28 +1100

    Change file type

--- src/LandingPage.jsx
+++ src/LandingPage.jsx
@@ -4,17 +4,22 @@ import Navbar from './Navbar';
 class LandingPage extends Component {
     render() {
         return (
+            <>
             <div>
               <Navbar />
               <h1>Welcome</h1>
               <p>Aut numquam tempora ab officia aliquam sit voluptatem dolore sit quasi minus. In dolores laudantium
                  ea quia quod et vero sint quo delen< br/>
                 iti explicabo sit debitis fugit. Non Quis quam et quia provident hic illo quis ab dolore excepturi.</p>
+            </div>
+            <div>
                 <h1>Case study 1</h1>
                 <p>Aut numquam tempora ab officia aliquam sit voluptatem dolore sit quasi minus. In dolores laudantium
                  ea quia quod et vero sint quo delen< br/>
                 iti explicabo sit debitis fugit. Non Quis quam et quia provident hic illo quis ab dolore excepturi.</p>
             </div>
+            </>
+  
         )
     }
 }
