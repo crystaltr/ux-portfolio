5cbf1fea92de12e11fb5aa2bd441137d585841bc
crystaltr <ctru0002@student.monash.edu>
2023-03-18 15:07:22 +1100

    added styling to navbar

--- src/LandingPage.jsx
+++ src/LandingPage.jsx
@@ -1,16 +1,14 @@
 import React, { Component } from 'react';
 import Navbar from './Navbar';
+import './landingpage.css';
 
 class LandingPage extends Component {
     render() {
         return (
             <>
-            <div>
+            <div className='headline'>
               <Navbar />
-              <h1>Welcome</h1>
-              <p>Aut numquam tempora ab officia aliquam sit voluptatem dolore sit quasi minus. In dolores laudantium
-                 ea quia quod et vero sint quo delen< br/>
-                iti explicabo sit debitis fugit. Non Quis quam et quia provident hic illo quis ab dolore excepturi.</p>
+              <h1>Crafting solutions < br/> for complex < br/> problems</h1>
             </div>
             <div>
                 <h1>Case study 1</h1>
--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,16 +1,23 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './navbar.css';
 
 const Navbar = () => {
     return (
+        <>
+        <div className='navbar'>
+        <div class ="rectangle"></div><div className='brandline'>Crystal Truong</div>
         <header>
              <nav>
                 <ul>
-                    <Link to="/">Home</Link> <br></br>
+                    <Link to="/">Home</Link>
                     <Link to="/about">About</Link>
+                    <a href="https://www.linkedin.com/in/crystal-truong-2334a2175/" target="_blank">Linkedin</a>
                 </ul>
             </nav>
         </header>
+        </div>
+        </>
     );
 };
 
